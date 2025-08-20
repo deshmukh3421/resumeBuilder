06b26226cb722997344b9d8bb6fa3c37016534a8
deshmukh3421 <udaydeshmukh1864@gmail.com>
2025-08-20 23:03:23 +0530

    First Commit

---
+++ frontend/resume-builder/src/pages/Auth/Login.jsx
@@ -0,0 +1,77 @@
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
+import Input from '../../components/Inputs/Input';
+import { validateEmail } from '../../utils/helper';
+
+const Login = ({setCurrentPage}) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+
+  const navigate = useNavigate();
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    if (!validateEmail(email)) {
+      setError("Please enter a valid email address.");
+      return;
+    }
+    if (!password) {
+      setError("Please enter the password");
+      return;
+    }
+
+    setError("");
+    //Login API Call 
+    try {
+
+    } catch (error) {
+
+    }
+  };
+
+  return (
+    <div className="w-[90vw] md:w-[33vw] p-7 flex flex-col justify-center">
+      <h3 className="text-lg font-semibold text-black">Welcome Back</h3> 
+      <p className="text-xs ☐text-slate-700 mt-[5px] mb-6">
+          Please enter your details to log in
+      </p>
+      <form onSubmit={handleLogin}>
+
+        <Input
+          value = {email}
+          onChange = {({target}) => setEmail(target.value)}
+          label = "Email Address"
+          placeholder = "Enter your email"
+          type = "text"
+        />
+
+        <Input
+          value = {password}
+          onChange = {({target}) => setPassword(target.value)}
+          label = "Password"
+          placeholder = "Enter your password"
+          type = "password"
+        />
+
+        {error && <p className="text-red-500 text-xs pb-2.5">{error}</p>}
+        <button type="submit" className="btn-primary">
+          LOGIN
+        </button>
+        <p className="text-[13px] text-slate-800 mt-3">
+          Don't have an account?{" "}
+          <button 
+            className="font-medium text-primary underline cursor-pointer"
+            onClick={() => {
+              setCurrentPage("signup");
+            }}
+          >
+            SignUp
+          </button>
+        </p>
+      </form>
+    </div>
+  )
+}
+
+export default Login
