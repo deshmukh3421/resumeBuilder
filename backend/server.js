9794863d59057ab3beb2680ffd87083f4056f492
deshmukh3421 <udaydeshmukh1864@gmail.com>
2025-08-28 17:18:32 +0530

    resumeRoutes and the image upload function

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,9 @@ const cors = require("cors");
 const path = require("path");
 const connectDB = require("./config/db");
 
+const authRoutes = require("./routes/authRoutes");
+const resumeRoutes = require("./routes/resumeRoutes");
+
 const app = express();
 
 // Middleware to handle CORS
@@ -19,6 +22,20 @@ connectDB();
 
 // Middleware
 app.use(express.json());
+
+//Routes
+app.use("/api/auth", authRoutes);
+app.use("/api/resume". resumeRoutes);
+
+// Serve uploads folder
+app.use(
+    "/uploads",
+    express.static (path.join(____dirname, "uploads"), {
+        setHeaders: (res, path) => {
+            res.set("Access-Control-Allow-Origin", "http://localhost:5173");
+        },
+    })
+);
 
 // Start Server
 const PORT = process.env.PORT || 5000;
